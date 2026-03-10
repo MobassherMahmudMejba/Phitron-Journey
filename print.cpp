f4782a3cc1ddfbd176a66de085cc4fa415173c79
Mobassher Mahmud Mejba <mobasshermahmudmejba@gmail.com>
2026-03-10 21:32:55 +0600

    Added my first C++ code files

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{ 
+    std :: cout <<"Hello World!\n";
+    int x = 100;
+    char c = 'M';
+    double d = 34.56;
+    std:: cout << x<<" " << c<<" " << d <<std::endl;
+    std:: cout << x<<std::endl << c<<std::endl << d<<std::endl;
+    std::cout<<"My favourite number is "<< x<<std::endl;
+    return 0;
+}
