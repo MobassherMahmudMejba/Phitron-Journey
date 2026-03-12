1167c8f7d5d36e96e88f2f7104509aab7d5f5173
Mobassher Mahmud Mejba <mobasshermahmudmejba@gmail.com>
2026-03-12 06:34:49 +0600

    Setprecision with example

---
+++ Setprecision.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip> // for setprecision
+using namespace std;
+int main() {
+    double pi = 3.14159265358979323846;
+
+    cout << "Default precision: " << pi << endl;
+
+    // Set precision to 2 decimal places
+    cout << fixed << setprecision(2);
+    cout << "Pi with 2 decimal places: " << pi << endl;
+
+    // Set precision to 4 decimal places
+    cout << setprecision(4);
+    cout << "Pi with 4 decimal places: " << pi << endl;
+
+    // Set precision to 6 decimal places
+    cout << setprecision(6);
+    cout << "Pi with 6 decimal places: " << pi << endl;
+
+    return 0;
+}
